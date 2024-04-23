de43002d86739bdf1da19b7533f7ee03d014da7b
abhijadhav03 <jadhavabhishek53366@gmail.com>
2024-04-24 01:21:07 +0530

    some data types

---
+++ datatype.js
@@ -0,0 +1,16 @@
+"use strict "; // treat all js code as newer version
+
+let name = "abhsihe"
+let age = 18
+let isLoggedIn =false
+
+// number => 2 to power 53
+//bigint
+// string => ""
+//boolean => true/false
+//null => stanalone value
+//undefined => not assigned value
+//symbol => unique 
+
+//object 
+console.log(typeof "abhishek");
--- index.js
+++ index.js
@@ -5,5 +5,10 @@ var accountPassword = "12345"
 accountCity = "bangalore"
 
 //accountId = 2//
-
+accountEmail = "abhi@gmail.com"
+accountPassword = "12345"
+accountCity = "Bengaluru"
 console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity])
+/* prefer not  to use var because of issue in block scope and functional scope*/
