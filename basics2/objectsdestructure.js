4b4403bb9cb8eedde098aabecf364b7b16d047f6
abhijadhav03 <jadhavabhishek53366@gmail.com>
2024-04-28 22:03:06 +0530

    json and obj destruct

---
+++ basics2/objectsdestructure.js
@@ -0,0 +1,29 @@
+const course = {
+    coursename: "javascript mastery",
+    price:"999",
+    courseInstructor: "hitesh"
+
+}
+
+//course.courseInstructor
+
+const {courseInstructor:  instructor} = course
+console.log(instructor);
+
+// const navbar = {} => {
+
+// }
+
+// navbar(company)
+
+// {
+//     "name": " abhishek",
+//     "coursename": "abhi now js"
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
