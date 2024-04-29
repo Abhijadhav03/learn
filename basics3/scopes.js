d7c657c541f358dfa283bf75c639e280477a3871
abhijadhav03 <jadhavabhishek53366@gmail.com>
2024-04-29 22:07:51 +0530

    scope block and mini hoisting

--- basics3/scopes.js
+++ basics3/scopes.js
@@ -4,7 +4,7 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20 
-    console.log("INNER: ", a);
+    //console.log("INNER: ", a);
    
 
 }
@@ -12,3 +12,42 @@ if (true) {
 console.log(a);
 //console.log(b);
 //console.log(c);
+
+function one(){
+    const username = "abhishek"
+
+    function two(){
+        const website = "youtube "
+        console.log(username);
+    }
+    //console.log(two.website);
+    two()
+}
+
+one()
+
+if (true) 
+{
+const username = "abhi"
+if (username === "abhi") {
+    const website = "youtube"
+   // console.log(username+website);
+
+    
+}   
+//console.log(website); 
+}
+//console.log(username);
+
+//+++++++++++++++ intersting +++++++++++++++++
+console.log(addone(5));
+function addone(num){
+    return num +1
+
+}
+
+
+const addTwo = function(num){
+    return num+2
+}
+addTwo(5)
