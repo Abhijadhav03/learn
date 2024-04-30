88f6aeb8eb6ad7295f8754d405d241640897f55e
abhijadhav03 <jadhavabhishek53366@gmail.com>
2024-04-30 23:17:11 +0530

    execution context

---
+++ controlflow/executioncntxt.js
@@ -0,0 +1,11 @@
+// 1. Global execution context => this 
+
+//2. memory phase 
+
+// 3. execution phase 
+
+
+//first in global execution context this is allocated and the var in this context is given as undefined
+//whereas the function is allocated as definition
+// if functions are there then memory phase and execution context of the function separate again
+//once the code is executed the context created by fuction is deleted
