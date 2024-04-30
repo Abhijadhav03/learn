a54d8835ff8b97c6af9eb9677fbf1b4a607205e6
abhijadhav03 <jadhavabhishek53366@gmail.com>
2024-04-30 23:30:01 +0530

    control flow

---
+++ controlflow/one.js
@@ -0,0 +1,24 @@
+//if 
+const isUserloggedIn = true
+const temperature = 41 
+
+if (temperature>50) {
+    console.log("leass than 50");
+
+    
+}else{
+console.log("temperature is greater than 50")
+}
+
+
+if( 2 === "2" ){
+console.log("executed");
+
+}
+else{
+    console.log("executed");
+}
+// 2<=2
+// 3!=2
+// <,>, <=, >= , ==, !=,===
+
